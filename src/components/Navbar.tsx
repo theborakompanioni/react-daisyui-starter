92eb1274eb5d9a9f222f9236d7492e805911accb
theborakompanioni <theborakompanioni+github@gmail.com>
2023-04-06 14:55:23 +0200

    chore: move NavLink instead of Link in navs

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,5 @@
 import { PropsWithChildren, ReactNode } from 'react'
-import { Link } from 'react-router-dom'
+import { Link, NavLink } from 'react-router-dom'
 import { WrenchScrewdriverIcon, InformationCircleIcon } from '@heroicons/react/24/outline'
 import { Navbar as DaisyNavbar, Button, Menu } from 'react-daisyui'
 import ROUTES from '../routes'
@@ -68,16 +68,16 @@ export function Navbar({ title, toggleSidebar }: NavbarProps) {
         <div className="flex-none hidden md:block">
           <Menu horizontal={true} className="gap-1">
             <Menu.Item>
-              <Link to={ROUTES.settings} className="rounded-lg">
+              <NavLink to={ROUTES.settings} className="rounded-lg">
                 <WrenchScrewdriverIcon className="w-6 h-6" />
                 Settings
-              </Link>
+              </NavLink>
             </Menu.Item>
             <Menu.Item>
-              <Link to={ROUTES.about} className="rounded-lg">
+              <NavLink to={ROUTES.about} className="rounded-lg">
                 <InformationCircleIcon className="w-6 h-6" />
                 About
-              </Link>
+              </NavLink>
             </Menu.Item>
           </Menu>
         </div>
@@ -85,85 +85,3 @@ export function Navbar({ title, toggleSidebar }: NavbarProps) {
     </DaisyNavbar>
   )
 }
-
-/*
-export function Sidebar({ elementId, title }: SidebarProps) {
-  const [visible, setVisible] = useState(false)
-
-  const toggleVisible = () => setVisible((current) => !current)
-
-  return (
-    <Drawer
-      side={true}
-      open={visible}
-      onClickOverlay={toggleVisible}
-      className="font-sans"
-    ><>
-      <Navbar>
-        <div className="flex-none lg:hidden">
-          <Button shape="square" color="ghost" onClick={toggleVisible}>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              className="inline-block w-6 h-6 stroke-current"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth={2}
-                d="M4 6h16M4 12h16M4 18h16"
-              />
-            </svg>
-          </Button>
-        </div>
-        <div className="flex-1 px-2 mx-2">Navbar Title</div>
-        <div className="flex-none hidden lg:block">
-          <Menu horizontal={true}>
-            <Menu.Item>
-              <a>Navbar Item 1</a>
-            </Menu.Item>
-            <Menu.Item>
-              <a>Navbar Item 2</a>
-            </Menu.Item>
-          </Menu>
-        </div>
-      </Navbar>
-      </>
-    </Drawer>)
-}*/
-/*
-  return (
-    <div
-      id={elementId}
-      className="absolute inset-y-0 left-0 transform -translate-x-full md:relative md:translate-x-0 transition duration-200 ease-in-out"
-    >
-      <FbSidebar>
-        <FbSidebar.Logo href="#" img="favicon.ico" imgAlt="">
-          <span data-testid="sidebar-title">{title}</span>
-        </FbSidebar.Logo>
-        <FbSidebar.Items>
-          <FbSidebar.ItemGroup>
-            <Link to={{ pathname: ROUTES.home }}>
-              <FbSidebar.Item as={'span'} icon={HomeIcon}>
-                Home
-              </FbSidebar.Item>
-            </Link>
-          </FbSidebar.ItemGroup>
-          <FbSidebar.ItemGroup>
-            <Link to={ROUTES.settings}>
-              <FbSidebar.Item as={'span'} icon={WrenchScrewdriverIcon}>
-                Settings
-              </FbSidebar.Item>
-            </Link>
-            <Link to={ROUTES.about}>
-              <FbSidebar.Item as={'span'} icon={InformationCircleIcon}>
-                About
-              </FbSidebar.Item>
-            </Link>
-          </FbSidebar.ItemGroup>
-        </FbSidebar.Items>
-      </FbSidebar>
-    </div>
-  )
-}*/
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,5 +1,5 @@
 import { PropsWithChildren, ReactNode } from 'react'
-import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 import { HomeIcon, WrenchScrewdriverIcon, InformationCircleIcon } from '@heroicons/react/24/outline'
 import { Menu } from 'react-daisyui'
 import ROUTES from '../routes'
@@ -11,114 +11,30 @@ interface SidebarProps {
 export function Sidebar({ title, children }: PropsWithChildren<SidebarProps>) {
   return (
     <div className="p-4 menu w-60 md:w-80 bg-base-100">
-      <Menu horizontal={false}>
+      <Menu horizontal={false} className="gap-1">
         <Menu.Title className="mb-2">
           <span>{title}</span>
         </Menu.Title>
         <Menu.Item>
-          <Link to={{ pathname: ROUTES.home }}>
+          <NavLink to={{ pathname: ROUTES.home }}>
             <HomeIcon className="w-6 h-6" />
             Home
-          </Link>
+          </NavLink>
         </Menu.Item>
         <Menu.Item>
-          <Link to={ROUTES.settings}>
+          <NavLink to={ROUTES.settings}>
             <WrenchScrewdriverIcon className="w-6 h-6" />
             Settings
-          </Link>
+          </NavLink>
         </Menu.Item>
-
         <Menu.Item>
-          <Link to={ROUTES.about}>
+          <NavLink to={ROUTES.about}>
             <InformationCircleIcon className="w-6 h-6" />
             About
-          </Link>
+          </NavLink>
         </Menu.Item>
       </Menu>
       <>{children}</>
     </div>
   )
 }
-
-/*
-export function Sidebar({ elementId, title }: SidebarProps) {
-  const [visible, setVisible] = useState(false)
-
-  const toggleVisible = () => setVisible((current) => !current)
-
-  return (
-    <Drawer
-      side={true}
-      open={visible}
-      onClickOverlay={toggleVisible}
-      className="font-sans"
-    ><>
-      <Navbar>
-        <div className="flex-none lg:hidden">
-          <Button shape="square" color="ghost" onClick={toggleVisible}>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              className="inline-block w-6 h-6 stroke-current"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth={2}
-                d="M4 6h16M4 12h16M4 18h16"
-              />
-            </svg>
-          </Button>
-        </div>
-        <div className="flex-1 px-2 mx-2">Navbar Title</div>
-        <div className="flex-none hidden lg:block">
-          <Menu horizontal={true}>
-            <Menu.Item>
-              <a>Navbar Item 1</a>
-            </Menu.Item>
-            <Menu.Item>
-              <a>Navbar Item 2</a>
-            </Menu.Item>
-          </Menu>
-        </div>
-      </Navbar>
-      <div className="flex h-36 items-center justify-center">Content</div>
-      </>
-    </Drawer>)
-}*/
-/*
-  return (
-    <div
-      id={elementId}
-      className="absolute inset-y-0 left-0 transform -translate-x-full md:relative md:translate-x-0 transition duration-200 ease-in-out"
-    >
-      <FbSidebar>
-        <FbSidebar.Logo href="#" img="favicon.ico" imgAlt="">
-          <span data-testid="sidebar-title">{title}</span>
-        </FbSidebar.Logo>
-        <FbSidebar.Items>
-          <FbSidebar.ItemGroup>
-            <Link to={{ pathname: ROUTES.home }}>
-              <FbSidebar.Item as={'span'} icon={HomeIcon}>
-                Home
-              </FbSidebar.Item>
-            </Link>
-          </FbSidebar.ItemGroup>
-          <FbSidebar.ItemGroup>
-            <Link to={ROUTES.settings}>
-              <FbSidebar.Item as={'span'} icon={WrenchScrewdriverIcon}>
-                Settings
-              </FbSidebar.Item>
-            </Link>
-            <Link to={ROUTES.about}>
-              <FbSidebar.Item as={'span'} icon={InformationCircleIcon}>
-                About
-              </FbSidebar.Item>
-            </Link>
-          </FbSidebar.ItemGroup>
-        </FbSidebar.Items>
-      </FbSidebar>
-    </div>
-  )
-}*/
